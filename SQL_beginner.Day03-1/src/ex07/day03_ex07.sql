33c018f5b11b49696a5130cf01c88e983df43740
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-23 20:42:04 +0300

    deleted

--- SQL_beginner.Day03-1/src/ex07/day03_ex07.sql
+++
@@ -1,2 +0,0 @@
-INSERT into menu (id, pizzeria_id, pizza_name, price) 
-values (19,2, 'greek pizza', 800);
--- SQL_beginner.Day03-1/src/ex08/day03_ex08.sql
+++
@@ -1,6 +0,0 @@
- insert into menu (id, pizzeria_id, pizza_name, price)  
- values ((select MAX(menu.id) from menu)+1,
-		 (select pizzeria.id from pizzeria
-					where pizzeria.name = 'Dominos'), 
-		 'sicilian pizza', 
-		 900);
