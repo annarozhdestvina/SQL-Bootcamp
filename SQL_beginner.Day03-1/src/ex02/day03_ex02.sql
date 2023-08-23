799d3047fff6ef60788696938a32fe77cffbf450
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-23 20:43:31 +0300

    deleted

--- SQL_beginner.Day03-1/src/ex02/day03_ex02.sql
+++
@@ -1,8 +0,0 @@
-with menu_cte as 
-(select * from menu
-where menu.id not in (select person_order.menu_id from person_order)
-order by menu.id)
-
-select menu_cte.pizza_name, menu_cte.price, pizzeria.name from menu_cte
-join pizzeria on menu_cte.pizzeria_id = pizzeria.id
-order by menu_cte.pizza_name, menu_cte.price
