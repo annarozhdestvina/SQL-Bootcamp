8b95f4142ac78ff6b2b9d89d97c36b3fbf377f79
annarozhdestvina <122921862+annarozhdestvina@users.noreply.github.com>
2023-08-23 20:31:57 +0300

    Delete SQL_beginner.Day05-1 directory

--- SQL_beginner.Day05-1/src/ex02/day05_ex02.sql
+++
@@ -1,3 +0,0 @@
-create index idx_person_name on person (upper(name));
-set enable_seqscan = off;
-explain analyze select person.id from person where name = 'NATALY';
--- SQL_beginner.Day05-1/src/ex04/day05_ex04.sql
+++
@@ -1,4 +0,0 @@
-create unique index idx_menu_unique on menu (pizzeria_id, pizza_name);
-set enable_seqscan = off;
-explain analyse select pizzeria_id, pizza_name from menu where pizzeria_id = 1 
-or pizza_name = 'supreme pizza';
