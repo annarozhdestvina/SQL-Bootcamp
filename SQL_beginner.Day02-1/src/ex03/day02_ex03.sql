c2af2b31e1def73bb2c36bb9fcd8788229f5ff21
annarozhdestvina <122921862+annarozhdestvina@users.noreply.github.com>
2023-08-23 20:31:32 +0300

    Delete SQL_beginner.Day02-1 directory

--- SQL_beginner.Day02-1/src/ex03/day02_ex03.sql
+++
@@ -1,9 +0,0 @@
-WITH TABLE_CTE AS (SELECT day::date AS missing_date
- 				FROM GENERATE_SERIES(timestamp '2022-01-01', '2022-01-10', '1 day') AS day)
-SELECT missing_date from TABLE_CTE
-LEFT JOIN  
-(SELECT visit_date from person_visits
-where person_id = 1 OR person_id = 2) q2
-ON missing_date = q2.visit_date
-WHERE visit_date IS NULL
-ORDER BY missing_date;
--- SQL_beginner.Day02-1/src/ex04/day02_ex04.sql
+++
@@ -1,8 +0,0 @@
-select 
-		menu.pizza_name as pizza_name
-	  , pizzeria.name as pizzeria_name
-	  , menu.price as price
-from menu
-join pizzeria on menu.pizzeria_id = pizzeria.id
-where pizza_name in ('mushroom pizza', 'pepperoni pizza')
-order by pizza_name, pizzeria_name
